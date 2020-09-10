9270a21ad8b0cf9c0b89f3ba7ab28a0401a3308f
N.Orsolya <solya1120@gmail.com>
2020-09-10 10:05:56 +0200

    Wares CRUD Java

---
+++ src/java/Controller/WaresController.java
@@ -0,0 +1,131 @@
+package Controller;
+
+import Service.WaresService;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.json.JSONArray;
+import org.json.JSONObject;
+public class WaresController extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("application/sjon;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            //Entity manager 
+            EntityManagerFactory emf = Persistence.createEntityManagerFactory("gazdalkodjokosanPU");
+            EntityManager em = emf.createEntityManager();
+            
+            //Wares service példány 
+            WaresService ws = new WaresService();
+            //Üres JSONobject 
+            JSONObject answer = new JSONObject();
+            
+            // Új áru hozzáadása
+            if(request.getParameter("task").equals("addNewWare")){
+             if(!request.getParameter("object").isEmpty() && !request.getParameter("price").isEmpty()) {
+                   String object = request.getParameter("object");
+                   Integer price = Integer.parseInt(request.getParameter("price"));
+                   
+                   if(ws.addNewWare(object, price, em)){
+                       answer.put("msg", "Sikeresen hozzáadtad a filmet");
+                   }
+                   else{
+                       answer.put("msg", "Hiba");
+                   }
+                   
+                }
+                else{
+                    answer.put("msg", "Hiba");
+                }
+                out.print(answer.toString());
+            } 
+            
+            //Összes áru kilistázása
+            if(request.getParameter("task").equals("selectAllWares")){
+                JSONArray result = ws.selectAllWares(em);
+                out.print(result.toString());
+            }
+            
+            //Áru módosítása
+            if(request.getParameter("task").equals("updateOneWare")){
+                   String object = request.getParameter("object");
+                   Integer price = Integer.parseInt(request.getParameter("price"));
+                   Integer id = Integer.parseInt(request.getParameter("id"));
+                   
+                   if(ws.updateOneWare(object, price, id, em)){
+                       answer.put("msg", "Sikeres módosítás");
+                   }
+                   else{
+                       answer.put("msg", "Hiba");
+                   }
+                   out.print(answer.toString());
+            }
+            
+            
+            //Áru törlése
+            if(request.getParameter("task").equals("deleteOneWare")){
+               Integer id = Integer.parseInt(request.getParameter("id"));
+               if(ws.deleteOneWare(id, em)){
+                    answer.put("msg", "Sikeres törlés");
+               }
+               else{
+                   answer.put("msg", "Hiba");
+               }
+               out.print(answer.toString());
+            }
+            
+            
+            
+            
+            
+            
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
--- src/java/Modell/Wares.java
+++ src/java/Modell/Wares.java
@@ -1,28 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Modell;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityManager;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
+import javax.persistence.ParameterMode;
+import javax.persistence.StoredProcedureQuery;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
+import org.json.JSONObject;
 
-/**
- *
- * @author solya
- */
 @Entity
 @Table(name = "wares")
 @XmlRootElement
@@ -85,7 +82,78 @@ public class Wares implements Serializable {
     public void setPrice(int price) {
         this.price = price;
     }
+    
+    
+    //////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////
+    
 
+    //Új áru hozzáadása
+    public static boolean addNewWare(String object, Integer price, EntityManager em){
+        try{
+            StoredProcedureQuery spq = em.createStoredProcedureQuery("addNewWare");
+            spq.registerStoredProcedureParameter("objectIN", String.class, ParameterMode.IN);
+            spq.registerStoredProcedureParameter("priceIN", Integer.class, ParameterMode.IN);
+            
+            spq.setParameter("objectIN", object);
+            spq.setParameter("priceIN", price);
+            
+            spq.execute();
+            em.close();
+            return true;
+        }
+        catch(Exception ex){
+            return false;
+        }
+    
+    }
+    
+    
+    
+    //Áruk kilistázása
+    
+    public static List<Wares> selectAllWares(EntityManager em){
+        List<Wares> wares = new ArrayList();
+        StoredProcedureQuery tarolt = em.createStoredProcedureQuery("selectAllWares");
+        List<Object[]> list = tarolt.getResultList();
+        for(Object[] ware : list){
+            int id= Integer.parseInt(ware[0].toString());
+            Wares w = em.find(Wares.class, id);
+            wares.add(w);
+        }
+        em.close();
+        return wares;
+    
+    }
+    
+    // JSON formátumu returnölés
+    public JSONObject toJson(){
+        JSONObject j = new JSONObject();
+        j.put("id",this.id);
+        j.put("object",this.object);
+        j.put("price",this.price);
+       
+        return j;
+    }
+    
+    //Áru törlése
+     public boolean deleteOneWare(Integer id, EntityManager em){
+        try{
+            StoredProcedureQuery spq = em.createStoredProcedureQuery("deleteOneWare");
+            spq.registerStoredProcedureParameter("idIN", Integer.class, ParameterMode.IN);
+            
+            spq.setParameter("idIN",id);
+            
+            spq.execute();
+            em.close();
+            return true;
+        }
+        catch(Exception ex){
+            return false;
+        }
+    }
+    
+    
     @Override
     public int hashCode() {
         int hash = 0;
